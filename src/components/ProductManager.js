b5d561b6102ce250dc88746040b33669e509d51d
MartinArielMendoza <marttx2022@gmail.com>
2023-03-28 23:35:48 -0300

    Implementando server

--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -57,22 +57,28 @@ class ProductManager {
     };
 }
 
-
-const productos = new ProductManager ();
-
-//productos.addProduct("producto prueba","Este es un producto prueba","200","Sin imagen","abc123","25")
-//productos.addProduct("producto prueba2","Este es un producto prueba2","300","Sin imagen","abc123","28")
-
+//productos.addProduct("producto prueba","Este es un producto prueba","200","Sin imagen1","abc123","1")
+//productos.addProduct("producto prueba2","Este es un producto prueba2","300","Sin imagen2","abc123","2")
+//productos.addProduct("producto prueba3","Este es un producto prueba3","200","Sin imagen3","abc123","3")
+//productos.addProduct("producto prueba4","Este es un producto prueba4","200","Sin imagen4","abc123","4")
+//productos.addProduct("producto prueba5","Este es un producto prueba5","200","Sin imagen5","abc123","5")
+//productos.addProduct("producto prueba6","Este es un producto prueba6","200","Sin imagen6","abc123","6")
+//productos.addProduct("producto prueba7","Este es un producto prueba7","200","Sin imagen7","abc123","7")
+//productos.addProduct("producto prueba8","Este es un producto prueba8","200","Sin imagen8","abc123","8")
+//productos.addProduct("producto prueba9","Este es un producto prueba9","200","Sin imagen9","abc123","9")
+//productos.addProduct("producto prueba10","Este es un producto prueba10","200","Sin imagen10","abc123","10")
 
 //productos.deleteProductsById(1)
 
-productos.updateProducts({
-    title: 'producto prueba2',
-    description:'Este es un producto prueba2',
-    price:'3000',
-    image:'Sin imagen',
-    code:'abc123',
-    stock:'28',
-    id:'2'
+//productos.updateProducts({
+    //title: 'producto prueba2',
+    //description:'Este es un producto prueba2',
+    //price:'3000',
+    //image:'Sin imagen',
+    //code:'abc123',
+    //stock:'28',
+    //id:'2'
 
-})
+//})
+
+export default ProductManager
